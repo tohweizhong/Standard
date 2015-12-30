5b9ebbba4dbd36c81e4c52b475c2dba288eb3873
tohweizhong <tohweizhong@hotmail.com>
2015-12-30 18:41:48 +0800

    301215 1845 @ made Cate2Prob() more efficient

--- codes/utils.R
+++ codes/utils.R
@@ -80,12 +80,19 @@ Cate2Prob <- function(one.col){
     if(!all(classes %in% unique(one.col))) stop("Something wrong with the classes")
     
     tmp.col <- NULL
-    # use a simple for-loop for now
-    for(i in seq(length(one.col))){
-        val <- one.col[i]
-        idx <- which(classes ==  val)
-        tmp.col <- c(tmp.col, tab[idx])
-    }
+#     # use a simple for-loop for now
+#     # very slow...
+#     for(i in seq(length(one.col))){
+#         val <- one.col[i]
+#         idx <- which(classes ==  val)
+#         tmp.col <- c(tmp.col, tab[idx])
+#     }
+    
+    tmp.col <- sapply(one.col, FUN = function(x){
+        idx <- which(classes == x)
+        return(tab[idx])
+    })
+    
     
     # check
     if(length(tmp.col) != length(one.col)) stop("Lengths dont tally")
