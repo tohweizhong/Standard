ea5af7420fcc230fc67be936a07e0dcd24ffbd97
tohweizhong <tohweizhong@hotmail.com>
2016-01-15 15:34:28 +0800

    150116 1535 @ implemented Cate2Chi()

---
+++ R/Cate2Chi.R
@@ -0,0 +1,50 @@
+# Function to convert a categorical variable to numerical,
+# replacing the values with chi-squared contributions
+
+# Inputs:
+# @ one.col: vector of categorical variable, may be factor or character
+# @ y: binary response variable may be factor or character
+
+Cate2Chi <- function(one.col, y){
+    
+    # check
+    if(length(unique(y)) != 2)
+        stop("Cate2Chi only works for binary response variables")
+    
+    # check
+    if(class(one.col) != "factor" && class(one.col) != "character")
+        stop("Cate2Chi only works for categorical variables")
+    
+    tab <- table(one.col, y)
+    chi_sq_test <- chisq.test(tab)
+    chi_sq <- chi_sq_test$statistic
+    
+    observed <- tab
+    expected <- chi_sq_test$expected
+    
+    chi_sq_contri <- ((observed - expected) ** 2) / expected
+    chi_sq_contri <- chi_sq_contri / chi_sq
+    
+    sum_across <- NULL
+    for(i in seq(nrow(chi_sq_contri))){
+        sum_across <- c(sum_across, chi_sq_contri[i,1] + chi_sq_contri[i,2])
+    }
+    names(sum_across) <- rownames(tab)
+    
+    tmp.col <- sapply(one.col, FUN = function(x){
+        idx <- which(names(sum_across) == x)
+        return(sum_across[idx])
+    })
+    
+    # check
+    if(length(tmp.col) != length(one.col)) stop("Lengths dont tally")
+    
+    return(tmp.col)
+}
+
+# example usage
+# df <- read.csv("data/adult.csv", stringsAsFactors = F)
+# str(df)
+# one.col <- df$education # 16 unique classes
+# y <- df$income
+# foo <- Cate2Chi(one.col = one.col, y = y)
