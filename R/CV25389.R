f3eaea445467ae4d38459658382d4fd616f065ed
tohweizhong <tohweizhong@hotmail.com>
2016-08-23 14:03:41 +0800

    230816 1400 @ updated CV25389

--- R/CV25389.R
+++ R/CV25389.R
@@ -1,8 +1,9 @@
 
 # modified function from stats.stackexchange
 # http://stats.stackexchange.com/questions/25389/obtaining-predicted-values-y-1-or-0-from-a-logistic-regression-model-fit/25398#25398
+# credits to Marco (http://stats.stackexchange.com/users/4856/macro)
 
-CV25389 <- function(pred_prob, truth){
+CV25389 <- function(pred_prob, truth, main = "Title"){
     
     perf <- function(cut, pred_prob, truth)
     {
@@ -21,13 +22,14 @@ CV25389 <- function(pred_prob, truth){
     s = seq(.01,.99,length=1000)
     OUT = matrix(0,1000,4)
     for(i in 1:1000) OUT[i,]=perf(s[i],pred_prob,truth)
-    plot(s,OUT[,1],xlab="Cutoff",ylab="Value",cex.lab=1.5,cex.axis=1.5,ylim=c(0,1),type="l",lwd=2,axes=FALSE,col=2)
+    plot(s,OUT[,1],xlab="Cutoff",ylab="Value",cex.lab=1.5,cex.axis=1.5,ylim=c(0,1),type="l",lwd=2,axes=FALSE,col=2,
+         main = main)
     axis(1,seq(0,1,length=5),seq(0,1,length=5),cex.lab=1.5)
     axis(2,seq(0,1,length=5),seq(0,1,length=5),cex.lab=1.5)
     lines(s,OUT[,2],col="darkgreen",lwd=2)
     lines(s,OUT[,3],col=4,lwd=2)
     lines(s,OUT[,4],col="darkred",lwd=2)
     box()
-    legend(0,.25,col=c(2,"darkgreen",4,"darkred"),lwd=c(2,2,2,2),c("Sensitivity","Specificity","Classification Rate","Distance"))
+    legend(0,.5,col=c(2,"darkgreen",4,"darkred"),lwd=c(2,2,2,2),c("Sensitivity","Specificity","Classification Rate","Distance"))
     
 }
